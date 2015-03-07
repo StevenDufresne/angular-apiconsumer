44ac65aa5920a65a61f652b7ba1fd42f83dcb255
Steve <dufresnesteven@gmail.com>
2015-03-07 15:22:36 -0500

    Initial commit

---
+++ apiConsumer.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('XXXX').factory('apiConsumer', [ '$http', '$q',
+	function($http, $q) {
+		var obj = {};
+
+		function _requestInfo(url) {
+			 var deferred = $q.defer();
+
+			$http.get(url)
+				.success(function (data) {
+					deferred.resolve(data);
+				})
+				.error(function (err) {
+					deferred.resolve(err);
+				});
+
+			return deferred.promise;
+		}
+
+		obj.apiName = {
+			getInfo: function (item) {
+				var url = '/api/url'
+
+				return _requestInfo(url);			
+			}
+		};
+
+		return obj;
+	}
+]);
